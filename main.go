2a99287f92bfc7e44387a30c6d96505e2f3f2133
le-garden-fox <le-garden-fox@example.com>
2020-09-27 10:05:02 -0700

    Add json middleware

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net"
 	"net/http"
@@ -11,8 +11,24 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// HealthCheckResponse base response
+type HealthCheckResponse struct {
+	Alive        bool
+	Host         string
+	Port         string
+	ErrorMessage string
+}
+
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+	router.Use(jsonContentTypeMiddleware)
 	headers := handlers.AllowedHeaders([]string{"X-Request-With", "Content-Type", "Authrization"})
 	methods := handlers.AllowedMethods([]string{"POST", "PUT", "GET", "DELETE", "OPTION"})
 	origins := handlers.AllowedOrigins([]string{"*"})
@@ -23,7 +39,13 @@ func main() {
 }
 
 func rootLink(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Alive"))
+
+	json.NewEncoder(w).Encode(HealthCheckResponse{
+		Alive:        true,
+		Host:         "",
+		Port:         "",
+		ErrorMessage: "",
+	})
 }
 
 func tcpHealthCheck(w http.ResponseWriter, r *http.Request) {
@@ -31,13 +53,25 @@ func tcpHealthCheck(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	port := params["port"]
 	host := params["host"]
+	var response HealthCheckResponse
 
 	timeout := time.Duration(1 * time.Second)
 	_, err := net.DialTimeout("tcp", host+":"+port, timeout)
 	if err != nil {
-		fmt.Fprintf(w, "%s %s %s\n", host, "not responding", err.Error())
+		response = HealthCheckResponse{
+			Alive:        false,
+			Host:         host,
+			Port:         port,
+			ErrorMessage: err.Error(),
+		}
 	} else {
-		fmt.Fprintf(w, "%s %s %s\n", host, "responding on port:", port)
+		response = HealthCheckResponse{
+			Alive:        true,
+			Host:         host,
+			Port:         port,
+			ErrorMessage: "",
+		}
 	}
 
+	json.NewEncoder(w).Encode(response)
 }
